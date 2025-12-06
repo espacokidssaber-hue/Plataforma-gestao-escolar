668b793d01fcaa39d8e72de38412a8acb9e6fd98
espacokidssaber-hue <espacokidssaber@gmail.com>
2025-12-06 06:53:47 -0300

    Enviando
    Enviando substituição de arquivos

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Application from './Application';
 
-// Vercel Build Hash: Force_Rebuild_v6_Lockfile_Fix
+// Vercel Build Hash: Force_Rebuild_v7_Critical_Fix
 const App: React.FC = () => {
   return (
     <Application />
