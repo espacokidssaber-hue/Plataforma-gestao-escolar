c6250669fb365d3545b464e541a09888abe1ba45
espacokidssaber-hue <espacokidssaber@gmail.com>
2025-12-06 07:02:17 -0300

    substituindo
    substituindo arquivos da plataforma

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Application from './Application';
 
-// Vercel Build Hash: Force_Rebuild_v7_Critical_Fix
+// Vercel Build Hash: Fix_ETARGET_and_ImportMap_v9
 const App: React.FC = () => {
   return (
     <Application />
